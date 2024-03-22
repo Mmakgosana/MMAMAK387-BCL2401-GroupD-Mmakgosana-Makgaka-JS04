eaaa9c01b5a41b55aef9e0ddf2c2d7d99821f5f8
Mmakgosana <makgakammakgosana@gmail.com>
2024-03-22 15:27:06 +0200

    I have completed second challenge

--- MMAMAK387-BCL2401-GroupD-Mmakgosana-Makgaka-JS04/index.js
+++ MMAMAK387-BCL2401-GroupD-Mmakgosana-Makgaka-JS04/index.js
@@ -4,15 +4,19 @@
 // Given a variable that holds a user's preferred type of Monster Energy drink, 
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
-let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+let userPreference = prompt("Enter your preffered type of Monster Energy drink"); // Possible values: 'Regular', 'Sugar-free', or undefined
+consol
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
-let cansLeft = 3; // Any number of cans
-console.log(``);
+////Assume cansLeft variable holds the number of Monster Energy drink cans left in the fridge
+let cansLeft = 25; // Let us assume there are 25 cans left
+//Use the ternary operator to determine the message based on the number of the cans left
+let message = (cansLeft<5) ? "Time to restock!" : "We're stocked!";
+//Print the message
+console.log(message);
 
 
 // #### Challenge 3: Workout Intensity
